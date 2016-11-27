8037c726da33cc6f1102f92a9b4dc9b318e4032b
podniesm <podniesm@gmail.com>
2016-11-27 15:14:37 +0100

    setup basic express

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+﻿const gulp = require('gulp');
+
+const paths = {
+    pages: ['src/*.html']
+};
+
+gulp.task("copy-html", function () {
+    return gulp.src(paths.pages)
+        .pipe(gulp.dest("_wwwroot"));
+});
+
+gulp.task('build', ['copy-html']);
