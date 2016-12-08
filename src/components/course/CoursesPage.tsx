276d6d6d6de6f403a9636d1483b1baa7aebd88a6
podniesm <podniesm@gmail.com>
2016-12-08 01:18:56 +0100

    Redxu-Flow +reducers

--- src/components/course/CoursesPage.tsx
+++ src/components/course/CoursesPage.tsx
@@ -1,14 +1,40 @@
 import IProps from '../common/IProps';
 import * as React from 'react';
 
-class CoursesPage extends React.Component<IProps, {}> {
+class CoursesPage extends React.Component<IProps, any> {
+    constructor(props: IProps, context?: any) {
+        super(props, context);
+        this.onTitleChange = this.onTitleChange.bind(this);
+        this.onClickSave = this.onClickSave.bind(this);
+        this.state = {
+            course: {
+                title: ''
+            }
+        };
+    }
+
     public render(): JSX.Element {
         return (
             <div>
                 <h1>Courses</h1>
+                <h2>Add Course</h2>
+                <input onChange={this.onTitleChange} value={this.state.course.title} />
+                <input type='submit' value='Save' onClick={this.onClickSave} />
             </div>
         );
-    } 
+    }
+
+    private onTitleChange(event: any): void {
+        const course = this.state.course;
+        course.title = event.target.value;
+        this.setState({
+            course: course
+        });
+    }
+
+    private onClickSave(): void {
+        alert(`Saving ${this.state.course.title}`);
+    }
 }
 
 export default CoursesPage;
